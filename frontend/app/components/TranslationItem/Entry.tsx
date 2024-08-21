8d6ea597ce5e3e546bae610da233be767d7b201a
Xinya Yang <xinyayang0506@gmail.com>
2024-08-22 00:01:36 +0800

    seperate the translation in each language to its own component

---
+++ frontend/app/components/TranslationItem/Entry.tsx
@@ -0,0 +1,89 @@
+import useConfirmationState from "@/app/hooks/useConfirmationState";
+import { Language } from "@/app/models/Language";
+import { Piece } from "@/app/models/Translation";
+import React from "react";
+import { Text, View } from "react-native";
+import ConfirmationButton from "./ConfirmationButton";
+import LanguageScroller from "./LanguageScroller";
+
+interface TranslationEntryProps {
+  toLang: Language;
+  translation: Piece;
+  entryIdx: number;
+  isFocused: boolean;
+  playAudio: (audio: string) => void;
+}
+
+enum Colors {
+  BACKGROUND = "bg-gray-50",
+  PRESSED = "bg-text-primary",
+  TEXT = "text-text-secondary",
+  TEXT_PRESSED = "text-background",
+}
+
+const Entry: React.FC<TranslationEntryProps> = ({
+  toLang,
+  translation,
+  entryIdx,
+  isFocused,
+  playAudio,
+}) => {
+  const {
+    selectedConfirmationLang,
+    isConfirmPressed,
+    handleConfirmPress,
+    handleConfirmRelease,
+    handleScroll,
+  } = useConfirmationState(translation.confirmations);
+
+  return (
+    <View key={`${toLang.name}-${entryIdx}`} className="mt-4">
+      <View
+        className={`flex-row items-start justify-between p-3 rounded-lg ${
+          isConfirmPressed ? Colors.PRESSED : Colors.BACKGROUND
+        }`}
+      >
+        <View className="flex-row items-start flex-1 mr-2">
+          <View className="flex-col items-start mr-3">
+            <Text
+              className={`text-lg font-semibold mb-2 ${
+                isConfirmPressed ? Colors.TEXT_PRESSED : "text-secondary-dark"
+              }`}
+            >
+              {toLang.acronym}
+            </Text>
+            {isFocused && (
+              <ConfirmationButton
+                isConfirmPressed={isConfirmPressed}
+                onPress={handleConfirmPress}
+                onRelease={handleConfirmRelease}
+                onMove={handleScroll}
+                playAudio={() => playAudio(translation.TTS)}
+              >
+                {isConfirmPressed && (
+                  <LanguageScroller
+                    confirmations={translation.confirmations}
+                    selectedLang={selectedConfirmationLang}
+                  />
+                )}
+              </ConfirmationButton>
+            )}
+          </View>
+          <Text
+            className={`${isFocused ? "text-lg" : "text-base"} ${
+              isConfirmPressed ? Colors.TEXT_PRESSED : Colors.TEXT
+            } flex-1`}
+          >
+            {isConfirmPressed &&
+            selectedConfirmationLang &&
+            translation.confirmations
+              ? translation.confirmations.get(selectedConfirmationLang)
+              : translation.text}
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Entry;
--- frontend/app/components/TranslationItem/TranslationItem.component.tsx
+++ frontend/app/components/TranslationItem/TranslationItem.component.tsx
@@ -1,23 +1,13 @@
 import { useAudio } from "@/app/hooks/useAudio";
-import useConfirmationState from "@/app/hooks/useConfirmationState";
-import { Language } from "@/app/models/Language";
-import { Piece, Translation } from "@/app/models/Translation";
-import React, { useCallback } from "react";
-import { Text, View } from "react-native";
-import ConfirmationButton from "./ConfirmationButton";
+import { Translation } from "@/app/models/Translation";
+import React from "react";
+import { View } from "react-native";
+import Entry from "./Entry";
 import InputSection from "./InputSection";
-import LanguageScroller from "./LanguageScroller";
 
 interface TranslationItemProps {
   item: Translation;
   isFocused: boolean;
-}
-
-enum Colors {
-  BACKGROUND = "bg-gray-50",
-  PRESSED = "bg-text-primary",
-  TEXT = "text-text-secondary",
-  TEXT_PRESSED = "text-background",
 }
 
 const TranslationItem: React.FC<TranslationItemProps> = ({
@@ -25,70 +15,6 @@ const TranslationItem: React.FC<TranslationItemProps> = ({
   isFocused,
 }) => {
   const { playAudio } = useAudio();
-
-  const renderTranslationEntry = useCallback(
-    ([toLang, translation]: [Language, Piece], entryIdx: number) => {
-      const {
-        selectedConfirmationLang,
-        isConfirmPressed,
-        handleConfirmPress,
-        handleConfirmRelease,
-        handleScroll,
-      } = useConfirmationState(translation.confirmations);
-
-      return (
-        <View key={`${toLang.name}-${entryIdx}`} className="mt-4">
-          <View
-            className={`flex-row items-start justify-between p-3 rounded-lg ${
-              isConfirmPressed ? Colors.PRESSED : Colors.BACKGROUND
-            }`}
-          >
-            <View className="flex-row items-start flex-1 mr-2">
-              <View className="flex-col items-start mr-3">
-                <Text
-                  className={`text-lg font-semibold mb-2 ${
-                    isConfirmPressed
-                      ? Colors.TEXT_PRESSED
-                      : "text-secondary-dark"
-                  }`}
-                >
-                  {toLang.acronym}
-                </Text>
-                {isFocused && (
-                  <ConfirmationButton
-                    isConfirmPressed={isConfirmPressed}
-                    onPress={handleConfirmPress}
-                    onRelease={handleConfirmRelease}
-                    onMove={handleScroll}
-                    playAudio={() => playAudio(translation.TTS)}
-                  >
-                    {isConfirmPressed && (
-                      <LanguageScroller
-                        confirmations={translation.confirmations}
-                        selectedLang={selectedConfirmationLang}
-                      />
-                    )}
-                  </ConfirmationButton>
-                )}
-              </View>
-              <Text
-                className={`${isFocused ? "text-lg" : "text-base"} ${
-                  isConfirmPressed ? Colors.TEXT_PRESSED : Colors.TEXT
-                } flex-1`}
-              >
-                {isConfirmPressed &&
-                selectedConfirmationLang &&
-                translation.confirmations
-                  ? translation.confirmations.get(selectedConfirmationLang)
-                  : translation.text}
-              </Text>
-            </View>
-          </View>
-        </View>
-      );
-    },
-    [isFocused, playAudio]
-  );
 
   return (
     <View
@@ -99,7 +25,18 @@ const TranslationItem: React.FC<TranslationItemProps> = ({
       }`}
     >
       <InputSection item={item} isFocused={isFocused} playAudio={playAudio} />
-      {Array.from(item.translations.entries()).map(renderTranslationEntry)}
+      {Array.from(item.translations.entries()).map(
+        ([toLang, translation], entryIdx) => (
+          <Entry
+            key={`${toLang.name}-${entryIdx}`}
+            toLang={toLang}
+            translation={translation}
+            entryIdx={entryIdx}
+            isFocused={isFocused}
+            playAudio={playAudio}
+          />
+        )
+      )}
     </View>
   );
 };
