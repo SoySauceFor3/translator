eb9191de94aa703292c8f1f130d1b46879bb13ce
Xinya Yang <xinyayang0506@gmail.com>
2024-08-22 01:55:59 +0800

    imrpove fakeAPIs, 1) add delays, 2) shorten the audio console log.

--- frontend/app/services/fakeApi.ts
+++ frontend/app/services/fakeApi.ts
@@ -1,33 +1,32 @@
 import { Language } from "@/app/models/Language";
-import birdSound from "@/assets/bird-sound.mp3";
-import { Audio } from "expo-av";
 
 export const fetchTranslation = async (
   sentence: string,
   outputLanguage: Language
 ): Promise<string> => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   return `fake translation for: ${sentence} to ${outputLanguage.name}`;
 };
 
 export async function fetchAudioBase64(base64Audio: string): Promise<string> {
   if (!base64Audio) return "";
-
   try {
-    const ssss = await Audio.Sound.createAsync(birdSound);
-    const status = await ssss.sound.getStatusAsync();
-    if (!status.isLoaded) {
-      throw new Error("Sound is not loaded");
-    }
-    const uri = status.uri as string;
-    const response = await fetch(uri);
-    const arrayBuffer = await response.arrayBuffer();
-    const base64String = btoa(
-      new Uint8Array(arrayBuffer).reduce(
-        (data, byte) => data + String.fromCharCode(byte),
-        ""
-      )
-    );
-    return base64String;
+    // const ssss = await Audio.Sound.createAsync(birdSound);
+    // const status = await ssss.sound.getStatusAsync();
+    // if (!status.isLoaded) {
+    //   throw new Error("Sound is not loaded");
+    // }
+    // const uri = status.uri as string;
+    // const response = await fetch(uri);
+    // const arrayBuffer = await response.arrayBuffer();
+    // const base64String = btoa(
+    //   new Uint8Array(arrayBuffer).reduce(
+    //     (data, byte) => data + String.fromCharCode(byte),
+    //     ""
+    //   )
+    // );
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    return "someBase64String";
   } catch (error) {
     console.error("Error loading MP3 file:", error);
     return "";
