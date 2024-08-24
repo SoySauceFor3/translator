5d572235e0c17eb258c60885b64b3daa9426e12a
Xinya Yang <xinyayang0506@gmail.com>
2024-08-25 00:04:37 +0800

    Add a X icon for clear the input

--- frontend/app/components/TranslatorCard/InputSection.tsx
+++ frontend/app/components/TranslatorCard/InputSection.tsx
@@ -9,6 +9,7 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons"; // Make sure you have this package installed
 import AudioRecorder from "./AudioRecorder";
 
 interface InputSectionProps {
@@ -18,27 +19,43 @@ interface InputSectionProps {
 const InputSection: React.FC<InputSectionProps> = ({ toLangs }) => {
   const { inputText, setInputText, inputRef } = useInputText();
   const { handleTranslateRequest } = useTranslator(toLangs);
+
   const handleTranslate = () => {
     Keyboard.dismiss();
     handleTranslateRequest(inputText);
+  };
+
+  const clearInput = () => {
+    setInputText("");
+    inputRef.current?.focus();
   };
 
   const isInputEmpty = !inputText.trim();
 
   return (
     <View className="mt-6">
-      <TextInput
-        ref={inputRef}
-        className="w-full h-32 text-lg text-text-primary bg-surface p-4 rounded-xl"
-        value={inputText}
-        onChangeText={setInputText}
-        onSubmitEditing={() => handleTranslateRequest(inputText)}
-        returnKeyType="default"
-        placeholder="Enter text to translate"
-        placeholderTextColor="#999"
-        multiline
-      />
-      <View className="mt-6 flex-row justify-between items-center ">
+      <View className="relative">
+        <TextInput
+          ref={inputRef}
+          className="w-full h-32 text-lg text-text-primary bg-surface p-4 rounded-xl pr-10"
+          value={inputText}
+          onChangeText={setInputText}
+          onSubmitEditing={() => handleTranslateRequest(inputText)}
+          returnKeyType="default"
+          placeholder="Enter text to translate"
+          placeholderTextColor="#999"
+          multiline
+        />
+        {!isInputEmpty && (
+          <TouchableOpacity
+            onPress={clearInput}
+            className="absolute right-2 top-2 p-2"
+          >
+            <Icon name="close-circle" size={24} color="#999" />
+          </TouchableOpacity>
+        )}
+      </View>
+      <View className="mt-6 flex-row justify-between items-center">
         <AudioRecorder />
         <TouchableOpacity
           className={`px-8 py-4 rounded-full shadow-md ${
