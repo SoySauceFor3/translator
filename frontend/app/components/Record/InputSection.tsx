9c302f355deba52f0ab34e00455faa95cbde855d
Xinya Yang <xinyayang0506@gmail.com>
2024-08-24 23:54:50 +0800

    Add longpress as copying text for Record.InputText

--- frontend/app/components/Record/InputSection.tsx
+++ frontend/app/components/Record/InputSection.tsx
@@ -1,8 +1,14 @@
 import { useInputText } from "@/app/contexts/InputTextContext";
 import { useAudio } from "@/app/hooks/useAudio";
 import { Record } from "@/app/models/Record";
-import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import {
+  Animated,
+  Clipboard,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 interface InputSectionProps {
@@ -13,21 +19,65 @@ interface InputSectionProps {
 export default function InputSection({ item, isFocused }: InputSectionProps) {
   const isTTSAvailable = item.input.TTS.trim() !== "";
   const { playAudio } = useAudio();
-  const { setInputText } = useInputText();
 
-  const handleLongPress = () => {
-    const text = item.input.text;
+  const text = item.input.text;
+
+  const { setInputText } = useInputText();
+  const putTextToInputWindow = () => {
     setInputText(text);
+  };
+
+  const [fadeAnim] = useState(new Animated.Value(0));
+  const copyText = () => {
+    const textToCopy = text;
+
+    Clipboard.setString(textToCopy);
+    console.log("copied");
+
+    Animated.sequence([
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 200,
+        useNativeDriver: true,
+      }),
+      Animated.delay(700),
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 200,
+        useNativeDriver: true,
+      }),
+    ]).start();
   };
 
   return (
     <View className="flex-row items-center justify-between mb-4 pb-3 border-b border-gray-200">
-      <TouchableOpacity onLongPress={handleLongPress} className="flex-1 mr-2">
+      <TouchableOpacity
+        activeOpacity={1}
+        onPress={putTextToInputWindow}
+        onLongPress={copyText}
+        className="flex-1 mr-2"
+      >
         <Text
           className={`${isFocused ? "text-2xl" : "text-xl"} font-bold text-text-primary`}
         >
           {item.input.text}
         </Text>
+        <Animated.View
+          style={{
+            opacity: fadeAnim,
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "rgba(0,0,0,0.3)",
+            borderRadius: 8,
+          }}
+        >
+          <Text className="text-white font-bold">Text copied to clipboard</Text>
+        </Animated.View>
       </TouchableOpacity>
       {isFocused && (
         <TouchableOpacity
